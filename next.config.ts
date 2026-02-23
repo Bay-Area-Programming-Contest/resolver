3c6571c81793447905ce4d086e13f7dac869d2f0
qpwoeirut <19750159+qpwoeirut@users.noreply.github.com>
2026-02-22 22:59:02 -0800

    try setting basePath to / to fix links

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 // Copied from https://github.com/gregrickaby/nextjs-github-pages
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/",
   images: {
     unoptimized: true,
   },
